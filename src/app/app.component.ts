42eb2a846768d62488c963d4d13b4c738def7a8a
Christos Tsortanidis <tsortanidis.ch@gmail.com>
2019-06-22 20:55:06 +0300

    wires cstsortan-components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Semester } from './models/semester';
 import { DataService } from './services/data.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { UserFacingContentFormComponent } from './user-facing-content-form/user-facing-content-form.component';
+import { Components } from 'cstsortan-components';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -11,9 +13,13 @@ import { UserFacingContentFormComponent } from './user-facing-content-form/user-
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  faBars = faBars;
+
   semesters$: Observable<Semester[]>;
 
   isAdmin$: Observable<boolean>;
+
+  @ViewChild('drawer', {static: false}) drawer: ElementRef<Components.SideDrawer>;
 
   constructor(
     private data: DataService,
@@ -21,6 +27,10 @@ export class AppComponent {
   ) {
     this.semesters$ = data.getSemesters();
     this.isAdmin$ = data.isAdmin();
+  }
+
+  openDrawer() {
+    this.drawer.nativeElement.openDrawer();
   }
 
   openUserFacingContentForm() {
